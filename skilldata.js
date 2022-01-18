a591ccbf0b87bc4fb285da15d5cd5a38221807f5
taichi3012 <68967462+taichi3012@users.noreply.github.com>
2022-01-18 22:01:08 +0900

    Revert "Use framework"

---
+++ skilldata.js
@@ -0,0 +1,100 @@
+const skill_data = [
+    {
+        "id": "general_attack",
+        "name": "スキルなし(通常攻撃)",
+        "selectorIndex": 0,
+        "multiply": 1.0,
+        "availabilSpecial": true
+    },
+    {
+        "id": "seiken_passive_boss",
+        "name": "下剋上(BOSS)",
+        "selectorIndex": 1,
+        "multiply": 1.2,
+        "availabilSpecial": true
+    },
+    {
+        "id": "seiken_passive_mob",
+        "name": "下剋上(MOB)",
+        "selectorIndex": 2,
+        "multiply": 0.7,
+        "availabilSpecial": true
+    },
+    {
+        "id": "volcano",
+        "name": "ボルケーノ",
+        "selectorIndex": 3,
+        "multiply": 22.0,
+        "availabilSpecial": false
+    },
+    {
+        "id": "magic_ball_chant",
+        "name": "マジックボール(詠唱あり)",
+        "selectorIndex": 4,
+        "multiply": 8.0,
+        "availabilSpecial": false
+    },
+    {
+        "id": "magic_ball_normal",
+        "name": "マジックボール(詠唱なし)",
+        "selectorIndex": 5,
+        "multiply": 4.0,
+        "availabilSpecial": false
+    },
+    {
+        "id": "shock_stone",
+        "name": "ショックストーン",
+        "selectorIndex": 6,
+        "multiply": 7.0,
+        "availabilSpecial": false
+    },
+    {
+        "id": "chaos_blizzard",
+        "name": "カオスブリザード(全弾Hit)",
+        "selectorIndex": 7,
+        "multiply": 7.0,
+        "availabilSpecial": true
+    },
+    {
+        "id": "snow_pillar",
+        "name": "雪柱",
+        "selectorIndex": 8,
+        "multiply": 4.0,
+        "availabilSpecial": false
+    },
+    {
+        "id": "over_shoot_shadow_power",
+        "name": "オーバーシュート(スキルあり)",
+        "selectorIndex": 9,
+        "multiply": 18.75,
+        "availabilSpecial": false
+    },
+    {
+        "id": "over_shoot_normal",
+        "name": "オーバーシュート(スキルなし)",
+        "selectorIndex": 10,
+        "multiply": 12.5,
+        "availabilSpecial": false
+    },
+    {
+        "id": "awakening",
+        "name": "覚醒",
+        "selectorIndex": 11,
+        "multiply": 2.0,
+        "availabilSpecial": true
+    },
+    {
+        "id": "blood_slash",
+        "name": "血の斬撃",
+        "selectorIndex": 12,
+        "multiply": 2.5,
+        "availabilSpecial": true
+    },
+    {
+        "id": "heiron_metu",
+        "name": "ヘイロン滅",
+        "selectorIndex": 13,
+        "multiply": 8.0,
+        "availabilSpecial": false
+    }
+];
