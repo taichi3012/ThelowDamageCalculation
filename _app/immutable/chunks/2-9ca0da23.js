47afca943af29f6372ee97dc6f40686d210f9aa3
taichi3012 <taichi3012@users.noreply.github.com>
2022-12-25 15:08:36 +0000

    deploy: 1f75c55b81bcc95b51cc01c08a591311494078f4

---
+++ _app/immutable/chunks/2-9ca0da23.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./_page-35e05a08.js";import{default as t}from"../components/pages/_page.svelte-1976bb04.js";export{t as component,r as universal};
--- _app/immutable/chunks/2-ae67efa1.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./_page-35e05a08.js";import{default as t}from"../components/pages/_page.svelte-afefd461.js";export{t as component,r as universal};
